a076d94c9142e9542c8d49e418836111483c61c1
polinamoskvina <160390879+polinamoskvina@users.noreply.github.com>
2024-06-05 12:36:24 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,18 +5,15 @@
 #include  <cstdlib>
 #include  "tree.h"
 
-std::vector<char> getPerm(const Tree& tree, int n) {
-std::vector<char> tmp_delete(std::vector<char> num, int ind_to_delete) {
+std::vector<char> temp_delete(std::vector<char> num, int ind_to_delete) {
     if (ind_to_delete >= 0 && ind_to_delete < num.size()) {
         num.erase(num.begin() + ind_to_delete);
     }
     return num;
 }
-
 std::vector<char> getPerm(const Tree& tree, int d) {
     std::vector<std::vector<char>> perms = tree.getPerms(3);
     if (perms.size() < d)
         return {};
     return perms[d - 1];
 }
-
